c10f0f159d9ec5f36a328f9b714da20f42d1793c
Julia Thea <yuliya.boyadjan@gmail.com>
2021-01-22 15:49:26 -0600

    modified entry file

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express'); 
+const path = require('path');
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+app.use(express.static(publicDirectoryPath));
+
+app.listen(process.env.PORT || 3000, () => {
+     console.log('Server is up on port 3000');
+})
