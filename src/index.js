357be87f5fbd8381335755667ae762514ecdb5ce
Julia Thea <yuliya.boyadjan@gmail.com>
2021-01-23 15:50:58 -0600

    added public files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express'); 
-const app = express();app.use(express.static('public'));
+const app = express();
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
      res.sendFile('index.html', {root: __dirname + '/public/'});
 });
